b5b1ca8f7f021b9deff2f454e46c378eac7e976d
nianjcn <nianjcn@gmail.com>
2014-02-12 18:20:12 +0800

    Revert "test on arguments of directive's compile & link fn"
    This reverts commit 05242274a1f5de5dd034ef2c0005c742d2734fb4.

--- app/js/directives.js
+++ app/js/directives.js
@@ -5,17 +5,7 @@
 
 angular.module('myApp.directives', []).
   directive('appVersion', ['version', function(version) {
-    return {
-    	template: '<a href="#">123</a>',
-    	replace: true,
-    	compile: function(te,ta,trs){
-    		console.log(te);
-    		console.log(ta);
-    		return function(s,le,la){
-    			console.log(s);
-    			console.log(le === te);
-    			console.log(la);
-    		}
-    	}
-    }
-  }])
+    return function(scope, elm, attrs) {
+      elm.text(version);
+    };
+  }]);
