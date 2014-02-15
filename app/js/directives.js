2a1c1e0112e2e644035810b66ffd099ede320f45
nainj <nainj@oupeng.com>
2014-02-15 15:32:57 +0800

    add index of directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -9,24 +9,24 @@ angular.module('myApp.directives', []).
     };
   }]).
   directive('jtParent',[function() {
-      console.log('=jt-Parent: This is warpper function=====================')
+      console.log('jt-Parent: This is warpper function')
       return {
           //template: 'Hello this is replaced parent content',
           compile: function(telm,tattrs,trunsclude){
-            console.log('==jt-Parent: This is compile function.=================')
+            console.log('jt-Parent: This is compile function of ' + tattrs.jtParentIndex)
             return  function(scope,ielm,iattrs){
-                      console.log('==jt-Parent: This is link function.=================')
+                      console.log('jt-Parent: This is link function of ' + iattrs.jtParentIndex)
                     }
           }
       }
   }]).
   directive('jtChild',[function(){
-      console.log('=jt-Child: This is warpper function=====================')
+      console.log('jt-Child: This is warpper function')
       return {
            compile: function(telm,tattrs,trunsclude){
-            console.log('==jt-Child: This is compile function.=================')
+            console.log('jt-Child: This is compile function of ' + tattrs.jtChildIndex)
             return  function(scope,ielm,iattrs){
-                      console.log('==jt-Child: This is link function.=================')
+                      console.log('jt-Child: This is link function of ' + iattrs.jtChildIndex)
                     }
           }
       }
